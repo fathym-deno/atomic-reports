92ce2ea9321acf57a1f080305b369ba6bb3d8949
mcgear <michael.gearhardt@gmail.com>
2024-01-08 12:51:45 -0700

    upgrades

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,6 +1,6 @@
 // export * from "../../atomic/mod.ts";
-export * from "https://deno.land/x/fathym_atomic@v0.0.93-integration/mod.ts";
-export * from "https://deno.land/x/fathym_common@v0.0.100/mod.ts";
+export * from "https://deno.land/x/fathym_atomic@v0.0.95/mod.ts";
+export * from "https://deno.land/x/fathym_common@v0.0.120/mod.ts";
 export { type ComponentChildren, type JSX } from "preact";
 export { useEffect, useState } from "preact/hooks";
 export {
