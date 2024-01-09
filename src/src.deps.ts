61ca0e6aea614aa68ab54d6c01ec6560e1fac1ff
mcgear <michael.gearhardt@gmail.com>
2024-01-09 00:59:32 -0700

    tailwind config

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,6 +1,3 @@
-// export * from "../../atomic/mod.ts";
-export * from "https://deno.land/x/fathym_atomic@v0.0.95/mod.ts";
-export * from "https://deno.land/x/fathym_common@v0.0.120/mod.ts";
 export { type ComponentChildren, type JSX } from "preact";
 export { useEffect, useState } from "preact/hooks";
 export {
@@ -9,3 +6,7 @@ export {
 } from "https://deno.land/x/fresh_charts@0.3.1/island.tsx";
 export { type ChartOptions } from "https://deno.land/x/fresh_charts@0.3.1/core.ts";
 export { render } from "https://deno.land/x/gfm@0.2.5/mod.ts";
+
+export * from "https://deno.land/x/fathym_common@v0.0.127/mod.ts";
+
+export * from "https://deno.land/x/fathym_atomic@v0.0.100/mod.ts";
