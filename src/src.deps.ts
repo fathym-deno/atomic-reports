0d5156ce1f581b5e9bb1695a22b4b09513ead3a7
mcgear <michael.gearhardt@gmail.com>
2024-03-18 08:01:59 -0600

    Ref updates and deno cleanup

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,5 +1,8 @@
-export { type ComponentChildren, type JSX } from "preact";
-export { useEffect, useState } from "preact/hooks";
+export {
+  type ComponentChildren,
+  type JSX,
+} from "https://esm.sh/preact@10.19.2";
+export { useEffect, useState } from "https://esm.sh/preact@10.19.2/hooks";
 export {
   Chart,
   type ChartType,
@@ -7,6 +10,6 @@ export {
 export { type ChartOptions } from "https://deno.land/x/fresh_charts@0.3.1/core.ts";
 export { render } from "https://deno.land/x/gfm@0.2.5/mod.ts";
 
-export * from "https://deno.land/x/fathym_common@v0.0.137/mod.ts";
+export * from "https://deno.land/x/fathym_common@v0.0.172/mod.ts";
 
-export * from "https://deno.land/x/fathym_atomic@v0.0.118/mod.ts";
+export * from "https://deno.land/x/fathym_atomic@v0.0.133/mod.ts";
